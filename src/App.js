e5a97f70a71c17af85172f17287f20750c856b9c
TanguyC0 <corman.tanguy.ct@gmail.com>
2022-11-17 15:20:49 +0100

    end init react project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React @ becode.org
         </a>
       </header>
     </div>
